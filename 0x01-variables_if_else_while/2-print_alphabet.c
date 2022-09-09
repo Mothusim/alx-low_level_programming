28afef9e15706f1148bab4580832942ae85a8f14
mothusim <mothusim97@gmail.com>
2022-09-09 04:27:31 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - prints alphabets in lower case
+*return: 0
+*/
+
+int main()
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
