4050a6e18638531642e57fa119b93a8e0efaa156
mothusim <mothusim97@gmail.com>
2022-09-09 04:47:46 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - prints the alphabet in lowercase, followed by a new line.
+*Return: 0
+*/
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		if (alphabet = 'q')
+			continue;
+		else if (alphabet = 'e')
+			continue;
+		putchar (alphabet);
+	putchar ('\n');
+	return (0);
+}
