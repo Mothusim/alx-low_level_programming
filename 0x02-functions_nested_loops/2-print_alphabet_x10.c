73b9b72b7f38809071f620731c690d45343be98f
mothusim <mothusim97@gmail.com>
2022-09-14 02:16:23 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*function that prints 10 times the alphabet
+*Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+	pritf('\n');
+}
