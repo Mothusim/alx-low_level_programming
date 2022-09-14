df95822872c705b05d621791e1e45b0e22f87a35
mothusim <mothusim97@gmail.com>
2022-09-14 02:02:24 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*main - Enrt point
+*Return: 0
+*/
+
+int void print_alphabet(void)
+{
+	char cha = 'G';
+
+	printf("%c", tolower(cha));
+	print('\n');
+	return (0);
+}
