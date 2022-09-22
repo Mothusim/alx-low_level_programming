fd7282608bedae74c3565d9cf28b833389db8cea
mothusim <mothusim97@gmail.com>
2022-09-21 17:49:09 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-*strcat-Concatenates the string pointed to by @src, including the terminat
+*strcat-Concatenates the string pointed to by @src
+*including terminate
 *null byte, to the end of the string pointed to by @dest
 *@dest: A pointer to the string to be concatenated upon
 *@src: The source string to be appended to @dest
